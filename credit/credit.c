d4e0fb5080bb9963485945cd89f13bae01136898
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 18:34:45 +0000

    Sat, Aug 10, 2024, 3:34 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -5,7 +5,7 @@ int main() {
     long int cardNumber = get_long("Number: ");
     int cksum1 = 0;
     int cksum2 = 0;
-    int multiplier = 1;
+    long int multiplier = 1;
     printf("%li\n", cardNumber);
     for(int i = 0; i < 8; i++) {
 
