0d79f7d930600e141a60c2c22aa71140b2a9680d
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 18:19:47 +0000

    Sat, Aug 10, 2024, 3:19 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -12,7 +12,7 @@ int main() {
     printf("%li\n", cardNumber%100000000/10000000);
     printf("%li\n", cardNumber%10000000000/1000000000);
     printf("%li\n", cardNumber%1000000000000/100000000000);
-    printf("%li\n", cardNumber%100000000000000/100000000000);
+    printf("%li\n", cardNumber%100000000000000/1000000000000);
     printf("%li\n", cardNumber%10000000000000000/10000000000000);
     return 0;
 }
