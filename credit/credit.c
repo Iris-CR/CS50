aa3fc865da8bc16bea004951d34860de11ea153a
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 18:15:51 +0000

    Sat, Aug 10, 2024, 3:15 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,14 +6,13 @@ int main() {
     int cksum1 = 0;
     int cksum2 = 0;
     printf("%li\n", cardNumber);
-    cksum1 += cardNumber%100/10;
-    cksum1 += cardNumber%10000/1000;
-    cksum1 += cardNumber%1000000/100000;
-    cksum1 += cardNumber%100000000/100000;
-    cksum1 += cardNumber%10000000000/10000000;
-    cksum1 += cardNumber%1000000000000/1000000000;
-    cksum1 += cardNumber%100000000000000/100000000000;
-    cksum1 += cardNumber%10000000000000000/10000000000000;
-    printf("%i\n", cksum1);
+    printf("%li\n", cardNumber%100/10);
+    printf("%li\n", cardNumber%10000/1000);
+    printf("%li\n", cardNumber%1000000/100000);
+    printf("%li\n", cardNumber%100000000/100000);
+    printf("%li\n", cardNumber%10000000000/10000000);
+    printf("%li\n", cardNumber%1000000000000/1000000000);
+    printf("%li\n", cardNumber%100000000000000/100000000000);
+    printf("%li\n", cardNumber%10000000000000000/10000000000000);
     return 0;
 }
