1059850c1eaf7b32abca3ba4fe434801c6164a56
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 18:09:38 +0000

    Sat, Aug 10, 2024, 3:09 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -3,7 +3,11 @@
 
 int main() {
     long int cardNumber = get_long("Number: ");
+    int cksum1 = 0;
+    int cksum2 = 0;
     printf("%li\n", cardNumber);
+    for(i = 1; i < 9; i++) {
+    }
     printf("%li\n", cardNumber%100/10);
     return 0;
 }
