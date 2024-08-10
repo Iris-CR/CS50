b50cea514c4450ad09355fc454d192ba7b082375
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 18:18:18 +0000

    Sat, Aug 10, 2024, 3:18 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -9,9 +9,9 @@ int main() {
     printf("%li\n", cardNumber%100/10);
     printf("%li\n", cardNumber%10000/1000);
     printf("%li\n", cardNumber%1000000/100000);
-    printf("%li\n", cardNumber%100000000/100000);
-    printf("%li\n", cardNumber%10000000000/10000000);
-    printf("%li\n", cardNumber%1000000000000/1000000000);
+    printf("%li\n", cardNumber%100000000/10000000);
+    printf("%li\n", cardNumber%10000000000/1000000000);
+    printf("%li\n", cardNumber%1000000000000/100000000000);
     printf("%li\n", cardNumber%100000000000000/100000000000);
     printf("%li\n", cardNumber%10000000000000000/10000000000000);
     return 0;
