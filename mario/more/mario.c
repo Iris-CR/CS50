ae678fbcf8453851d1af465d05de119ab52b55c9
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 17:24:24 +0000

    Sat, Aug 10, 2024, 2:24 PM -03:00

--- mario/more/mario.c
+++ mario/more/mario.c
@@ -7,7 +7,7 @@ int main() {
         height = get_int("Height: ");
     } while(height < 1 || height > 8);
     for(int i = 0; i < height; i++){
-        for(int j = 0; j < height-1; j++) {
+        for(int j = 0; j < height-1-i; j++) {
             printf(" ");
         }
         for(int k = 0; k <= i; k++) {
