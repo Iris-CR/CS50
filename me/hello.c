e6c5229ae554221ced45287c3759a40f3e7ba4c4
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 16:55:51 +0000

    Sat, Aug 10, 2024, 4:55 PM +00:00

--- me/hello.c
+++ me/hello.c
@@ -2,8 +2,7 @@
 #include <cs50.h>
 
 int main() {
-    string name;
-    printf("What's your name? ");
-    
+    string name = get_string("What's your name? ");
+    printf("hello, %s\n", name);
     return 0;
 }
