814b537263981297238c90368c49458aca9a9288
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 18:51:30 +0000

    Sat, Aug 10, 2024, 3:51 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,6 +6,7 @@ int main() {
     int cksum1 = 0;
     int cksum2 = 0;
     int temp = 0;
+    int digitCount = 0;
     long int multiplier = 1;
     printf("%li\n", cardNumber);
     for(int i = 0; i < 8; i++) { // Picks numbers skiping a decimal place
@@ -15,14 +16,16 @@ int main() {
         }
         cksum1 += temp;
         multiplier *= 100;
+        digitCount++;
     }
     multiplier = 1;
     for(int i = 0; i < 8; i++) { // Picks numbers skiping a decimal place starting with the last
         cksum2 += cardNumber%(10*multiplier)/(1*multiplier);
         multiplier *= 100;
+        digitCount++;
     }
     if((cksum1 + cksum2) % 10 == 0) {
-        
+
     }
     return 0;
 }
