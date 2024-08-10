8f537d6ae0eda0de3d176b83394eaceee23be063
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 19:04:32 +0000

    Sat, Aug 10, 2024, 4:04 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -6,7 +6,7 @@ int main() {
     int cksum1 = 0;
     int cksum2 = 0;
     int temp = 0;
-    int digitCount = 0;
+    int cardT = 0; // 1 - Visa | 2 - MasterCard | 3 - AMEX
     long int multiplier = 1;
     printf("%li\n", cardNumber);
     for(int i = 0; i < 8; i++) { // Picks numbers skiping a decimal place
@@ -22,9 +22,12 @@ int main() {
         cksum2 += cardNumber%(10*multiplier)/(1*multiplier);
         multiplier *= 100;
     }
-    if(cardNumber%(100*10000000000000000)/(10*10000000000000000) != 0)
+    if(cardNumber%(100*10000000000000000)/(10*10000000000000000) == 4) {
+        cardT = 1;
+    } else if(cardNumber%(100*10000000000000000)/(10*10000000000000000) == 5 && (cardNumber%(10*10000000000000000)/(1*10000000000000000) > 0) && (cardNumber%(10*10000000000000000)/(1*10000000000000000) > 0))
     if((cksum1 + cksum2) % 10 == 0) {
 
     }
     return 0;
 }
+
