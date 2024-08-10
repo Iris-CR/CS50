4d3996ae60917301abea9674f2ed564bb954d2ff
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-10 18:37:09 +0000

    Sat, Aug 10, 2024, 3:37 PM -03:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,18 +7,10 @@ int main() {
     int cksum2 = 0;
     long int multiplier = 1;
     printf("%li\n", cardNumber);
-    for(int i = 0; i < 8; i++) {
-
-        printf("%li\n", cardNumber%(100*multiplier)/(10*multiplier));
+    for(int i = 0; i < 8; i++) { // Picks numbers skiping a decimal place
+        cksum1 += cardNumber%(100*multiplier)/(10*multiplier);
         multiplier *= 100;
     }
-    printf("%li\n", cardNumber%100/10);
-    printf("%li\n", cardNumber%10000/1000);
-    printf("%li\n", cardNumber%1000000/100000);
-    printf("%li\n", cardNumber%100000000/10000000);
-    printf("%li\n", cardNumber%10000000000/1000000000);
-    printf("%li\n", cardNumber%1000000000000/100000000000);
-    printf("%li\n", cardNumber%100000000000000/10000000000000);
-    printf("%li\n", cardNumber%10000000000000000/1000000000000000);
+    printf("%i", cksum1);
     return 0;
 }
