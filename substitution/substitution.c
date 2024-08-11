d9bef3e33e158d39b0ffc6832cc97f342d708457
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-11 16:53:11 +0000

    Sun, Aug 11, 2024, 1:53 PM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -17,6 +17,9 @@ int main(int argc, string argv[]) {
         return 1;
     } else {
         for(int i = 0; argv[1][i] != '\0'; i++) {
+            if(!isalpha(argv[1][i])) {
+                return 1;
+            }
             if(letters[1][i] != '0') {
                 return 1;
             } else {
