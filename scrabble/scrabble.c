aca7fed603a033a104b6ad6f4468e5a64d77e37d
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-11 14:11:17 +0000

    Sun, Aug 11, 2024, 11:11 AM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <ctype.h>
+#include <string.h>
 
 int main(void) {
     int scores[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     string pl1 = get_string("Player 1: ");
     string pl2 = get_string("Player 2: ");
-    pl1 = toupper(pl1);
-    pl2 = toupper(pl2);
+    for(int i = 0, j = strlen(pl1); i < j; i++) {
+        pl1[i] = toupper(pl1[i]);
+    }
+    for(int i = 0, j = strlen(pl2); i < j; i++) {
+        pl2[i] = toupper(pl2[i]);
+    }
     printf("%s\n", pl1);
     printf("%s\n", pl2);
     return 0;
