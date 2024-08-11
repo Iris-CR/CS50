38dd91571a572d2fbc99275f5f13620a1cd60bcd
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-11 15:52:16 +0000

    Sun, Aug 11, 2024, 12:52 PM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <string.h>
 #include <cs50.h>
 
 int main(int argc, string argv[]) {
-    
+    if(argc != 2) {
+        printf("Usage: ./substitution key\n");
+    } else if(strlen(argv[1]) != 26) {
+        printf("Key must contain 26 characters.\n");
+    } else {
+        printf("Correct\n");
+    }
     return 0;
 }
