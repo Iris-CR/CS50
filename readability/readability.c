ad415d0bda33811ae90829c0625f9275d81e6452
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-11 15:17:53 +0000

    Sun, Aug 11, 2024, 12:17 PM -03:00

--- readability/readability.c
+++ readability/readability.c
@@ -2,12 +2,14 @@
 #include <cs50.h>
 #include <ctype.h>
 
+float averageBy100(int a, int b);
+
 int main(void) {
     string text = get_string("Text: ");
     int letters = 0;
     int words = 0;
     int sent = 0;
-    float CLI = 0;
+    int CLI = 0;
     for(int i = 0; text[i] != '\0'; i++) {
         if(isalpha(text[i])) {
             letters++;
@@ -18,11 +20,15 @@ int main(void) {
             sent++;
         }
     }
-    CLI = 0.0588 * letters/words * 100.0 - 0.296 * sent/words * 100.0 - 15.8;
+    CLI = 0.0588 * averageBy100(letters, words) - 0.296 * averageBy100(sent, words) - 15.8;
     if (CLI < 1) {
         printf("Before Grade 1\n");
     } else if (CLI > 16) {
         printf("Grade 16+\n");
-    } else printf("Grade %f\n", CLI);
+    } else printf("Grade %i\n", CLI);
     return 0;
+}
+
+float averageBy100(int a, int b) {
+    return 100.0 * a/b;
 }
