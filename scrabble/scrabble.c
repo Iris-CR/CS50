184caed0ba6931f067f80525d83a478db22e7f06
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-11 14:15:04 +0000

    Sun, Aug 11, 2024, 11:15 AM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -5,11 +5,14 @@
 
 int main(void) {
     int scores[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+    int plScore[2] = {0, 0};
     string pl1 = get_string("Player 1: ");
     string pl2 = get_string("Player 2: ");
     for(int i = 0, j = strlen(pl1); i < j; i++) {
         pl1[i] = toupper(pl1[i]);
-        pl1[i]-65
+        for(int k = 0; k < 26; k++) {
+            if (pl1[i]-65 == scores[])
+        }
     }
     for(int i = 0, j = strlen(pl2); i < j; i++) {
         pl2[i] = toupper(pl2[i]);
