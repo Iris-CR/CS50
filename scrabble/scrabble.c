886815070331d9f13cb312ad4f069fdfdbc58895
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-11 14:07:50 +0000

    Sun, Aug 11, 2024, 11:07 AM -03:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <cs50.h>
+#include <ctype.h>
 
 int main(void) {
+    int scores[26] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     string pl1 = get_string("Player 1: ");
     string pl2 = get_string("Player 2: ");
+    pl1 = toupper(pl1);
+    pl2 = toupper(pl2);
+    printf("%s\n", pl1);
+    printf("%s\n", pl2);
     return 0;
 }
