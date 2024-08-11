2288b4ea01d625952fd77761c9700b4a24d505c1
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-11 16:41:11 +0000

    Sun, Aug 11, 2024, 1:41 PM -03:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,7 +6,6 @@
 int main(int argc, string argv[]) {
     char letters[26][26] = {{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'},
     {'0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'}};
-    bool toTest = true;
     string plainTxt;
     string cipherTxt;
 
@@ -16,15 +15,16 @@ int main(int argc, string argv[]) {
     } else if(strlen(argv[1]) != 26) {
         printf("Key must contain 26 characters.\n");
         return 1;
-    } else if(toTest) {
+    } else {
         for(int i = 0; argv[i][i] != '\0'; i++) {
             if(letters[i][i] == '0') {
                 letters[i][i] = argv[i][i];
             } else if(argv[i][i] == '\0') {
-                toTest = false;
+                break;
             } else return 1;
         }
     }
+
     plainTxt = get_string("plaintext:  ");
     cipherTxt = plainTxt;
     for(int i = 0; plainTxt[i] != '\0'; i++) {
