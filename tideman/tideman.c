cd67a5e006500e7062d29daed55aba5e7ff7738c
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-12 18:27:05 +0000

    Mon, Aug 12, 2024, 3:27 PM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -152,17 +152,14 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
-    int diff;
-    bool nswitch = 1;
-    while (nswitch)
+    int diff = 0;
+    for (int i = 0; i < pair_count; i++)
     {
-        diff = 0;
-        for (int i = 0; i < pair_count; i++)
+        for (int j = 0; j < pair_count; j++)
         {
             if (diff < pairs[i].winner - pairs[i].loser)
             {
                 diff = pairs[i].winner - pairs[i].loser;
-                nswitch = 0;
             }
         }
     }
