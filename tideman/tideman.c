f59bddd9375f56093fae7e8eacdba0d1d8589b99
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-12 13:30:00 +0000

    Mon, Aug 12, 2024, 10:30 AM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -103,7 +103,7 @@ bool vote(int rank, string name, int ranks[])
     {
         if (strcmp(name, candidates[i]) == 0)
         {
-            ranks[rank]++;
+            ranks[rank] = 1;
             return true;
         }
     }
