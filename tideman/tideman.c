e7859a4fea6de1254f007349cc5bc4d310266aac
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-12 14:03:42 +0000

    Mon, Aug 12, 2024, 11:03 AM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -113,7 +113,10 @@ bool vote(int rank, string name, int ranks[])
 // Update preferences given one voter's ranks
 void record_preferences(int ranks[])
 {
-    ranks[0]
+    for (int i = 0; i < candidate_count; i++)
+    {
+        ranks[i];
+    }
     return;
 }
 
