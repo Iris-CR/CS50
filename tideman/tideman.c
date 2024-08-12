21a8e19ab3431b45375e9f6edea5982b70688cac
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-12 15:45:47 +0000

    Mon, Aug 12, 2024, 12:45 PM -03:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -88,15 +88,7 @@ int main(int argc, string argv[])
 
         printf("\n");
     }
-    for (int i = 0; i < candidate_count; i++)
-    {
-        for (int j = 0; j < candidate_count; j++)
-        {
-            printf("%i ", preferences[i][j]);
-        }
-        printf("\n");
-    }
-
+    
     add_pairs();
     sort_pairs();
     lock_pairs();
