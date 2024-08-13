a83f323ff9d2763713427fac755b69141af2adcf
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-13 16:06:54 +0000

    Tue, Aug 13, 2024, 1:06 PM -03:00

--- filter-less/filter-less/helpers.c
+++ filter-less/filter-less/helpers.c
@@ -19,6 +19,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             image[y][x].rgbtGreen = round(av);
             image[y][x].rgbtRed = round(av);
         }
+        av = 0;
     }
     return;
 }
