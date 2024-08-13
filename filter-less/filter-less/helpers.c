ccad039cab3f54917269a4c9c3b25bba458498ef
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-13 13:27:28 +0000

    Tue, Aug 13, 2024, 10:27 AM -03:00

--- filter-less/filter-less/helpers.c
+++ filter-less/filter-less/helpers.c
@@ -3,15 +3,19 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    float av = 0;
     for (int y = 0; y < height; y++)
     {
         for (int x = 0; x < width; x++)
         {
-            // Pick the average of RGB for each pixel
-            image[y][x].rgbtBlue;
-            image[y][x].rgbtGreen;
-            image[y][x].rgbtRed;
-            // Update Pixel
+            av += image[y][x].rgbtBlue;
+            av += image[y][x].rgbtGreen;
+            av += image[y][x].rgbtRed;
+            av /= 3.0;
+
+            image[y][x].rgbtBlue = (int)av;
+            image[y][x].rgbtGreen (int)av;
+            image[y][x].rgbtRed av;
         }
     }
     return;
