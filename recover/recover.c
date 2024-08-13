f696e750449f0c41dce43649db80f18ae8bdb994
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-13 18:28:26 +0000

    Tue, Aug 13, 2024, 3:28 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -16,12 +16,17 @@ int main(int argc, char *argv[])
         return 2;
     }
     BYTE buffer[512];
-    int 
+    int imgCount = 0;
+    char *outName ;
+    FILE *out = fopen()
     while (fread(&buffer, sizeof(buffer), 1, infile))
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] >= 0xe0 && buffer[3] <= 0xef)
         {
-
+            if (imgCount == 0)
+            {
+                fwrite(&buffer, sizeof(buffer), 1, out)
+            }
         }
     }
 }
