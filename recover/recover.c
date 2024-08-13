26a01d1b6002604a1a94ad8a83bb70057100380f
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-13 18:02:59 +0000

    Tue, Aug 13, 2024, 3:02 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -8,6 +8,15 @@ int main(int argc, char *argv[])
     if (argc != 2) {
         return 1;
     }
-    char *s = argv[1];
+    char *infile = argv[1];
     FILE *f = fopen(s, "r");
+    if (f == NULL)
+    {
+        printf("Could not open %s.\n", infile);
+        return 4;
+    }
+    while (fread())
+    {
+
+    }
 }
