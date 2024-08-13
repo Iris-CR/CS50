409e139d8ba75e978f4889bc86f306afcea86348
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-13 13:42:39 +0000

    Tue, Aug 13, 2024, 10:42 AM -03:00

--- filter-less/filter-less/helpers.c
+++ filter-less/filter-less/helpers.c
@@ -26,18 +26,12 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
     float sepiaRed, sepiaGreen, sepiaBlue;
+    int ogRed, ogGreen, ogBlue;
     for (int y = 0; y < height; y++)
     {
         for (int x = 0; x < width; x++)
         {
-            av += image[y][x].rgbtBlue;
-            av += image[y][x].rgbtGreen;
-            av += image[y][x].rgbtRed;
-            av /= 3.0;
-
-            image[y][x].rgbtBlue = round(av);
-            image[y][x].rgbtGreen = round(av);
-            image[y][x].rgbtRed = round(av);
+            ogRed = 
         }
     }
     return;
