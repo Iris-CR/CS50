b7301059a8ce2f8ee58c50e13a82d547c04aa210
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-13 18:46:20 +0000

    Tue, Aug 13, 2024, 3:46 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -7,6 +7,7 @@ typedef uint8_t BYTE;
 int main(int argc, char *argv[])
 {
     if (argc != 2) {
+        printf("Usage: ./recover rawfile\n");
         return 1;
     }
     char *fileName = argv[1];
