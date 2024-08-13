88f45bdcf59844ec2541d841fbe2da4cf8672d30
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-13 18:20:17 +0000

    Tue, Aug 13, 2024, 3:20 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -15,9 +15,12 @@ int main(int argc, char *argv[])
         printf("Could not open %s.\n", infile);
         return 2;
     }
-    BYTE buffer[];
-    while (fread(&buffer, sizeof(buffer), 1, infile))
+    BYTE buffer[4];
+    while (fread(&buffer, sizeof(BYTE)*512, 1, infile))
     {
-
+        if ()
+        {
+            buffer[0] == 
+        }
     }
 }
