cbaaf328e5ce1d106c20d16c808a8e7e819c4b41
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-13 18:52:24 +0000

    Tue, Aug 13, 2024, 3:52 PM -03:00

--- recover/recover.c
+++ recover/recover.c
@@ -20,10 +20,10 @@ int main(int argc, char *argv[])
     BYTE buffer[512];
     int imgCount = 0;
     char outName[8];
-    FILE *out == NULL;
+    FILE *out = NULL;
     while (fread(&buffer, sizeof(buffer), 1, infile))
     {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] >= 0xe0 && buffer[3] <= 0xef)
+        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3]&0xf0) == 0xe0)
         {
             if (imgCount == 0)
             {
