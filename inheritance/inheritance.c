ea742731ccd21d444743388dadc53122631275ae
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-15 01:30:26 +0000

    Wed, Aug 14, 2024, 10:30 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -39,7 +39,7 @@ int main(void)
 person *create_family(int generations)
 {
     // TODO: Allocate memory for new person
-    person persPtr = malloc(sizeof(person));
+    person *persPtr = malloc(sizeof(person));
 
     // If there are still generations left to create
     if (generations > 1)
@@ -78,10 +78,17 @@ void free_family(person *p)
 {
     // TODO: Handle base case
     if (p->parents[0] == NULL && p->parents[1] == NULL)
+    {
+        free(p);
+        return;
+    }
 
     // TODO: Free parents recursively
+    free_family(p->parents[0]);
+    free_family(p->parents[1]);
 
     // TODO: Free child
+    free_family(p);
 }
 
 // Print each family member and their alleles.
