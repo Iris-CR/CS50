2de866f6183b127d4ea920850d0971da97e034cb
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-15 01:37:32 +0000

    Wed, Aug 14, 2024, 10:37 PM -03:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -77,9 +77,8 @@ person *create_family(int generations)
 void free_family(person *p)
 {
     // TODO: Handle base case
-    if (p->parents[0] == NULL && p->parents[1] == NULL)
+    if (p == NULL)
     {
-        free(p);
         return;
     }
 
@@ -88,7 +87,7 @@ void free_family(person *p)
     free_family(p->parents[1]);
 
     // TODO: Free child
-    free_family(p);
+    free(p);
 }
 
 // Print each family member and their alleles.
