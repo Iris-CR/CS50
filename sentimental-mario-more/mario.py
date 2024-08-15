e66d75954e2077293b9fad0caa993ba9680db9d5
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-15 23:17:33 +0000

    Thu, Aug 15, 2024, 8:17 PM -03:00

--- sentimental-mario-more/mario.py
+++ sentimental-mario-more/mario.py
@@ -1,10 +1,15 @@
 import cs50
 
-
 while True:
-    height = get_int("Height: ")
+    height = cs50.get_int("Height: ")
     if height > 0 and height < 9:
         break
 for i in range(height):
-    for j in range(height-1):
-        print(" "end="")
+    for j in range(height-1-i):
+        print(" ", end="")
+    for k in range(i):
+        print("#", end="")
+    print("  ", end="")
+    for l in range(i):
+        print("#", end="")
+    print()
