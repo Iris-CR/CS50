f7b3a6c93e1f66f209677c4f8cd31c167c6c2b62
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-15 12:40:11 +0000

    Thu, Aug 15, 2024, 9:40 AM -03:00

--- speller/dictionary.c
+++ speller/dictionary.c
@@ -41,6 +41,11 @@ bool load(const char *dictionary)
     {
         return false;
     }
+    char *word;
+    while(fscanf(file, "%s", word) != "EOF")
+    {
+
+    }
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
