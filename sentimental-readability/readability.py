9f03084569eafbc6f79f0ba804dc8f3f42ea77c6
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-19 14:12:25 +0000

    Mon, Aug 19, 2024, 11:12 AM -03:00

---
+++ sentimental-readability/readability.py
@@ -0,0 +1,14 @@
+import cs50
+
+
+def main():
+    text = cs50.get_string("Text: ")
+    letters = 0
+    words = 0
+    sent = 0
+    cli = 0
+    grade = 0
+
+
+def average_by_100(a, b):
+    return 100 * a / b
