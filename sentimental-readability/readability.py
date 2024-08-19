4368251d3f3518d047b44113440f7b1dc2756c81
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-19 14:22:41 +0000

    Mon, Aug 19, 2024, 11:22 AM -03:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -6,8 +6,22 @@ def main():
     letters = 0
     words = 0
     sent = 0
-    cli = 0
-    grade = 0
+    for i in range(0, len(text)):
+        if text[i].isalpha():
+            letters += 1
+        elif text[i] == ' ':
+            words += 1
+        elif text[i] == '.' or text[i] == '!' or text[i] == '?':
+            sent +=1
+    words += 1
+    cli = 0.0588 * average_by_100(letters, words) - 0.296 * average_by_100(sent, words) - 15.8
+    grade = int(cli)
+    if cli < 1:
+        print("Before Grade 1")
+    elif (CLI > 16):
+        print("Grade 16+\n")
+    else:
+        print("Grade", grade)
 
 
 def average_by_100(a, b):
