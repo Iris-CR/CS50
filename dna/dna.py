607b53324347a6863217dcb45e9cdbc6b083dd27
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-19 15:46:14 +0000

    Mon, Aug 19, 2024, 12:46 PM -03:00

--- dna/dna.py
+++ dna/dna.py
@@ -9,7 +9,7 @@ def main():
         return
 
     # TODO: Read database file into a variable
-    with open(sys.argv[2]) as file:
+    with open(sys.argv[1]) as file:
         reader = csv.DictReader(file)
         print(reader.fieldnames)
 
