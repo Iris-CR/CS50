6e6e422ed34df3b635bb2ec53a30c8e84fc800ff
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-19 15:50:04 +0000

    Mon, Aug 19, 2024, 12:50 PM -03:00

--- dna/dna.py
+++ dna/dna.py
@@ -9,8 +9,12 @@ def main():
         return
 
     # TODO: Read database file into a variable
+    dnas = []
     with open(sys.argv[1]) as fdb:
         header = csv.DictReader(fdb)
+        for row in header:
+            dnas.append(row)
+            print(row)
         print(header.fieldnames)
 
     # TODO: Read DNA sequence file into a variable
