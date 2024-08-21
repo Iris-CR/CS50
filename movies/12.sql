af8eb8bb2e370949b3f2cb394bf181d3761ffbe4
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-21 15:40:21 +0000

    Wed, Aug 21, 2024, 12:40 PM -03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,5 +1,7 @@
 SELECT title FROM movies WHERE id IN
 (SELECT movie_id FROM stars WHERE person_id IN
  (SELECT id FROM people WHERE name = 'Bradley Cooper'))
- (SELECT id FROM people WHERE name = 'Jennifer Lawrence')
+ AND
+(SELECT movie_id FROM stars WHERE person_id IN
+ (SELECT id FROM people WHERE name = 'Jennifer Lawrence'));
 
