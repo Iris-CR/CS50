2183ee6defc3dd17dd32436ccb724789dda291f0
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-21 15:33:38 +0000

    Wed, Aug 21, 2024, 12:33 PM -03:00

--- movies/11.sql
+++ movies/11.sql
@@ -3,5 +3,5 @@ WHERE id IN
 (SELECT movie_id FROM ratings WHERE movie_id IN
  (SELECT movie_id FROM stars WHERE person_id IN
   (SELECT id FROM people WHERE name = 'Chadwick Boseman')))
-  ORDER BY rating DESC;
+  ORDER BY rating DESC LIMIT 5;
 
