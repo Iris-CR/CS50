b7f4e2fdfe39fa0a5ae42d06ba43d788e28a62f6
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-21 15:13:51 +0000

    Wed, Aug 21, 2024, 12:13 PM -03:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,5 @@
-SELECT name FROM movies WHERE id IN
-(SELECT movie_id FROM ratings WHERE movie)
-SELECT id FROM people WHERE name = 'Chadwick Boseman'
+SELECT title FROM movies WHERE id IN
+(SELECT movie_id FROM ratings WHERE movie_id IN
+ (SELECT movie_id FROM stars WHERE person_id IN
+  (SELECT id FROM people WHERE name = 'Chadwick Boseman')));
+
