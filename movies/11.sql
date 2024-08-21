31490471799c7b5badcbccae64c241fb2a9d842f
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-21 15:32:57 +0000

    Wed, Aug 21, 2024, 12:32 PM -03:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,6 +1,7 @@
-SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id ORDER BY rating DESC
+SELECT title FROM movies JOIN ratings ON movies.id = ratings.movie_id
 WHERE id IN
 (SELECT movie_id FROM ratings WHERE movie_id IN
  (SELECT movie_id FROM stars WHERE person_id IN
-  (SELECT id FROM people WHERE name = 'Chadwick Boseman')));
+  (SELECT id FROM people WHERE name = 'Chadwick Boseman')))
+  ORDER BY rating DESC;
 
