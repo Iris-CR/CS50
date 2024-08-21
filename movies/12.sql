06807ecab633ee58a7950855aaa160dc7dd486e6
Iris-CR <81541917+Iris-CR@users.noreply.github.com>
2024-08-21 16:30:36 +0000

    Wed, Aug 21, 2024, 1:30 PM -03:00

--- movies/12.sql
+++ movies/12.sql
@@ -1,7 +1,7 @@
 SELECT title FROM movies WHERE id IN
 (SELECT movie_id FROM stars WHERE person_id IN
  (SELECT id FROM people WHERE name = 'Bradley Cooper'))
- AND
+ AND id IN
 (SELECT movie_id FROM stars WHERE person_id IN
  (SELECT id FROM people WHERE name = 'Jennifer Lawrence'));
 
